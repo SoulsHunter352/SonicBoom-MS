75a9c8ad4c371603f9698c48894c453abe531064
noname <dm.dr@mail.ru>
2024-12-09 11:08:36 +0300

    ура победа

--- backend/Api/api/api/settings.py
+++ backend/Api/api/api/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 from pathlib import Path
 from datetime import timedelta
+from django.conf.global_settings import MEDIA_ROOT
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
@@ -34,6 +35,7 @@ INSTALLED_APPS = [
     'support.apps.SupportConfig',
     # 'rest_framework_simplejwt',
     'rest_framework.authtoken',
+    'users.apps.UsersConfig',
     'content.apps.ContentConfig'
 ]
 MIDDLEWARE = [
