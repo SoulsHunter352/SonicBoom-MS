5ab275ce4ce04bacaa8df615f9d3da277e810dff
SoulsHunter352 <vovamaiorov11@gmail.com>
2024-12-18 00:02:09 +0300

    Добавленый пути в urls.py, запись cookie для access токена

--- backend/Api/api/users/urls.py
+++ backend/Api/api/users/urls.py
@@ -10,8 +10,10 @@ router.register(r'users', views.UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('login/', views.login_view, name='login'),
+    path('login/', views.login, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('check_auth/', views.check_auth, name='check_auth'),
+    path('refresh_access_token/', views.refresh_access_token, name='refresh_access_token')
 ]
